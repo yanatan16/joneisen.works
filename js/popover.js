326de20b400047263f34482f795cf3f113b5abf4
Jon Eisen <jon@joneisen.me>
2015-10-30 14:45:46 -0600

    popovers work on firefox

--- js/popover.js
+++ js/popover.js
@@ -6,15 +6,13 @@
             Array.prototype.forEach.call(items, function (item) {
                 var div = document.createElement('div')
                 div.classList.add("popover-box")
-                div.innerText = item.title
+                var title = div.innerHTML = item.getAttribute('title')
                 div.style.opacity = 0
                 div.style.position = "absolute"
                 var rect = item.getBoundingClientRect()
                 div.style.top = rect.bottom + "px"
                 div.style.left = rect.left + "px"
                 item.title = ""
-
-                console.log(div.innerText, rect)
 
                 document.body.appendChild(div)
 
