cbfc00917fae53abb05c23eb3e0ddb8dac0655a2
Jon Eisen <jon@joneisen.me>
2015-11-07 23:45:04 -0700

    dont use popover on mobile

--- js/popover.js
+++ js/popover.js
@@ -1,28 +1,40 @@
 (function () {
     document.addEventListener('DOMContentLoaded', function () {
-        setTimeout(function () {
-            var items = document.querySelectorAll('a.popover[title]')
+        function isMobile() {
+            var w = window,
+                d = document,
+                e = d.documentElement,
+                g = d.getElementsByTagName('body')[0],
+                x = w.innerWidth || e.clientWidth || g.clientWidth
 
-            Array.prototype.forEach.call(items, function (item) {
-                var div = document.createElement('div')
-                div.classList.add("popover-box")
-                var title = div.innerHTML = item.getAttribute('title')
-                div.style.opacity = 0
-                div.style.position = "absolute"
-                var rect = item.getBoundingClientRect()
-                div.style.top = (rect.bottom + window.scrollY) + "px"
-                div.style.left = (rect.left + window.scrollX) + "px"
-                item.title = ""
+            return x < 750
+        }
 
-                document.body.appendChild(div)
+        if (!isMobile()) {
+            setTimeout(function () {
+                var items = document.querySelectorAll('a.popover[title]')
 
-                item.addEventListener('mouseover', function () {
-                    div.style.opacity = 1
-                })
-                item.addEventListener('mouseleave', function () {
+                Array.prototype.forEach.call(items, function (item) {
+                    var div = document.createElement('div')
+                    div.classList.add("popover-box")
+                    var title = div.innerHTML = item.getAttribute('title')
                     div.style.opacity = 0
+                    div.style.position = "absolute"
+                    var rect = item.getBoundingClientRect()
+                    div.style.top = (rect.bottom + window.scrollY) + "px"
+                    div.style.left = (rect.left + window.scrollX) + "px"
+                    item.title = ""
+
+                    document.body.appendChild(div)
+
+                    item.addEventListener('mouseover', function () {
+                        div.style.opacity = 1
+                    })
+                    item.addEventListener('mouseleave', function () {
+                        div.style.opacity = 0
+                    })
                 })
-            })
-        }, 500)
+            }, 500)
+        }
     })
 })()
