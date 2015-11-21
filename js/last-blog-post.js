8e7c3b8dd6dba315354f3a8ef16543025b2761dd
Jon Eisen <jon@joneisen.me>
2015-11-21 00:14:23 -0700

    add a last blog post link

---
+++ js/last-blog-post.js
@@ -0,0 +1,23 @@
+(function () {
+    function getLastBlogTitle(cb) {
+        var req = {url: 'http://joneisen.me/last-post-title.txt'}
+        nanoajax.ajax(req, function (statusCode, body, request) {
+            if (statusCode == 200) {
+                var split = body.split('|*|')
+                cb(split[0], split[1])
+            }
+            // silently fail
+        })
+    }
+
+    function appendLastBlogPost(title, link) {
+        var top = document.querySelector('.last-blog-post'),
+            a = document.querySelector('.last-blog-post .last-blog-title')
+
+        a.innerText = title
+        a.href = link
+        top.style.display = 'inherit'
+    }
+
+    getLastBlogTitle(appendLastBlogPost)
+})()
